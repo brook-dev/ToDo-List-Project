8ff217df090b2abaf1b4b2fcdeba064c77ca1fcb
SamaiAlthiabat <skyalprogrammer@gmail.com>
2021-02-05 17:04:00 -0500

    updated forms and validations

--- js/index.js
+++ js/index.js
@@ -1,41 +1,16 @@
-let taskName = document.getElementById('taskName');
-let taskDescription = document.getElementById('taskDescription');
-let assignedTo = document.getElementById('assignedTo');
-let dueDateId = document.getElementById('dueDateId');
-let taskStatusId = document.getElementById('taskStatusId');
-let submitButton = document.getElementById('submitButton');
+function listener(){
+    const taskName = document.getElementById('taskName').value;
+    console.log("Task Name:  "+taskName);
 
+    const taskDescription = document.getElementById('taskDescription').value;
+    console.log("Task Description:  "+taskDescription);
+     
+    const assignedTo = document.getElementById('assignedTo').value;
+    console.log("Assigned To:  "+assignedTo);
 
+    const dueDateId = document.getElementById("dueDateId").value;
+    console.log("Due Date:  "+dueDateId);
 
-taskName.addEventListener('taskName', (e) => {
-    e.preventDefault();
-    if (!textField.value) {
-        console.log('Task Name:   ' + taskName);
-    }
-})
-
-taskDescription.addEventListener('taskDescription', (x) => {
-    x.preventDefault();
-    if (!textField.value) {
-        console.log('Task Description:   ' + taskDescription);
-    }
-})
-assignedTo.addEventListener('assignedTo', (y) => {
-    y.preventDefault();
-    if (!textField.value) {
-        console.log('Assigned To:   ' + assignedTo);
-    }
-})
-
-dueDateId.addEventListener('dueDateId', (z) => {
-    z.preventDefault();
-    if (!textField.value) {
-        console.log('Due Date:   ' + dueDateId);
-    }
-})
-taskStatusId.addEventListener('taskStatusId', (f) => {
-    f.preventDefault();
-    if (!textField.value) {
-        console.log('Task Status:   ' + taskStatusId);
-    }
-})
+    const taskStatus = document.getElementById("taskStatus").value;
+    console.log("Task Status:  "+taskStatus);
+}
