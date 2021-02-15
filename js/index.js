c7119f7e88b5cb1d3560de38bca95bc6b67fa2dd
Bchiksa <brookjig@gmail.com>
2021-02-15 10:15:38 -0500

    tasks 6 t0 9

--- js/index.js
+++ js/index.js
@@ -1,18 +1,72 @@
-function listener(){
-    const taskName = document.getElementById('taskName').value;
-    console.log("Task Name:  "+taskName);
-
-    const taskDescription = document.getElementById('taskDescription').value;
-    console.log("Task Description:  "+taskDescription);
-     
-    const assignedTo = document.getElementById('assignedTo').value;
-    console.log("Assigned To:  "+assignedTo);
-
-    const dueDateId = document.getElementById("dueDateId").value;
-    console.log("Due Date:  "+dueDateId);
-
-    const taskStatus = document.getElementById("taskStatus").value;
-    console.log("Task Status:  "+taskStatus);
-
+const taskManager = new TaskManager()
+taskManager.load()
+taskManager.render()
+const validFormFieldInput = () => {
+    const newTaskNameInput = document.querySelector('#task-name')   ;
+    const name = newTaskNameInput.value;
+    const newDescriptionInput = document.querySelector('#description');
+    const description = newDescriptionInput.value;
+    const newAssignedToInput = document.querySelector('#assigned-to');
+    const assignedTo = newAssignedToInput.value;
+    const newDueDateInput = document.querySelector('#due-date');
+    const dueDate = newDueDateInput.value;
+    if (name===""){
+        $('#name-alert').show()
+        setTimeout(()=>{$('#name-alert').hide()},2000)
+        return false;
+    }
+    if (description==="") {
+        $('#descrip-alert').show()
+        setTimeout(()=>{$('#descrip-alert').hide()},2000)
+        return false;
+    }
+    if (assignedTo===""){
+        $('#assigned-alert').show()
+        setTimeout(()=>{$('#assigned-alert').hide()},2000)
+        return false;
+    }
+    if (dueDate === ""){
+        $('#due-alert').show()
+        setTimeout(()=>{$('#due-alert').hide()},2000)
+        return false;
+    }
+    taskManager.addTask(name,description,assignedTo,dueDate)
+    taskManager.save()
+    taskManager.render()
+    newTaskNameInput.value = ""
+    newDescriptionInput.value = ""
+    newAssignedToInput.value = ""
+    newDueDateInput.value = ""
 }
+const tasksList = document.querySelector('#tasks');
+tasksList.addEventListener('click',(event) => {
+        const parentTask = event.target.parentElement.parentElement.parentElement
+        const taskId = parentTask.dataset.taskId
+        const task = taskManager.getTaskById(taskId)
+    if (event.target.classList.contains('done-button')){
+        task.status = "DONE"
+        taskManager.save()
+        taskManager.render()
+        return false;
+    }
+    if (event.target.classList.contains('delete-button')){
+        taskManager.deleteTask(taskId)
+        taskManager.save()
+        taskManager.render()
+        return false;
+    }
+})
+//     //  In index.js, add an event listener to the New Task form, listening to the submit event. If there is already an event listener used for validation, use that one.
+// const sub = document.getElementById('submitButton');
 
+//  sub.addEventListener('click', listener);
+
+ // Create a taskHtml variable with the result of calling the createTaskHtml function, making sure to pass a value for each parameter.
+// const taskHtml = createTaskHtml('finalProject', 'the should meet to review on task 5,6 and 7', 'Group', '2/9/2021', 'review');
+
+//TaskManager.addTask(taskName, taskDescription, assignedTo, dueDateId, taskStatus);
+//TaskManager.render(); // ****** I didnt see the tasks are added on the lists @ this stage!
+//  console.log(tasksList);
+
+// console.log(createHtml);
+// task 7:step 2====> Add an Event Listener to the Task List, listening for the 'click' event.
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,8 +1,111 @@
-class taskManager {
-    constructor() {
-        this.task = [];
-    }
-}
+              
+// In js/taskManager.js, above the TaskManager class definition, create a new function, createTaskHtml. The function should accept the following parameters:
+const createTaskHtml = (name,description,assignedTo,dueDate,status,id) => { 
+  const display = status == "TODO" ? "" : "d-none";   
+  const color = status == "TODO" ? "btn-outline-warning" : "btn-success"
+  const html = `<li class="list-group-item">
+                <div class="task" data-task-id=${id}>
+                  <div class="task-top">
+                    <div>
+                      <span class="task-title"><strong> Task Name: </strong>${name}</span>
+                    </div>
+                    <div>
+                        <span class="btn btn-pill ${color} task-status">${status}</span>
+                    </div>
+                  </div>
+                  <div class="task-desc">
+                    <p><strong> Description: </strong>${description}</p>
+                  </div>
+                  <div>
+                    <span class="task-assign"><strong> Assigned to: </strong>: ${assignedTo}</span>
+                  </div>
+                  <div>
+                    <span class="task-date"><strong> Due to: </strong> ${dueDate}</span>
+                  </div>
+                  <div class="task-bottom">
+                    <div>
+                      <span class="${display} btn btn-outline-success done-button">Mark as done</span>
+                    </div>
+                    <div>
+                      <span class="btn btn-outline-danger delete-button">Delete</span>
+                    </div>
+                  </div>
+                </div>
+              </li>`
+  return html;  }
+ class TaskManager {
+      constructor(currentId=0) {
+          this.tasks = []
+          this._currentId=currentId
+      }
+      addTask(name,description,assignedTo,dueDate,status="TODO") {
+          this.tasks.push({
+              id: this._currentId,
+              name: name,
+              description: description,
+              assignedTo: assignedTo,
+              dueDate: dueDate,
+              status: status
+          })
+          this._currentId++;
+      }
+      render() {
+          const tasksHtmlList = []
+          this.tasks.forEach((e,i)=>{
+              e.id=i
+              const date = new Date(e.dueDate);
+              const formattedDate = date.toDateString();
+              const taskHtml = createTaskHtml(e.name,e.description,e.assignedTo,formattedDate,e.status,i);
+              tasksHtmlList.push(taskHtml);
+          })
+          const tasksHtml = tasksHtmlList.join("\n");
+          $('#tasks').html(tasksHtml)
+      }
+      getTaskById(taskId) {
+          let foundTask;
+          this.tasks.forEach(e=>{
+              const task = e
+              if (task.id==taskId){
+                  foundTask = task
+                  }
+          })
+          return foundTask
+      }
+      save() {
+          const tasksJson = JSON.stringify(this.tasks)
+          localStorage.setItem('tasks',tasksJson)
+          localStorage.setItem('currentID',JSON.stringify(this._currentId))
+      }
+      load() {
+          if (localStorage.getItem('tasks') !== null){
+              const tasksJson = localStorage.getItem('tasks')
+              this.tasks = JSON.parse(tasksJson)
+          }
+          if (localStorage.getItem("currentID") !== null){
+              this._currentId = +localStorage.getItem('currentID')
+          }
+      }
+      deleteTask (taskId) {
+          this.tasks.splice(taskId,1)
+          this._currentId--
+      }
+  }
+  
+  module.exports = TaskManager
 
-let tasks = new taskManager();
-console.log(tasks);
+//TaskManager() = document.getElementsByClassName('display').innerHtml;
+
+  
+//   // task 5 task
+// With Jorge
+//  addTask(taskName, taskDescription, assignedTo, dueDateId, taskStatus="TODO") {
+//   this.task.push({
+//        id:this.currentId,
+//        taskName: document.getElementById('taskName').innerHTML,
+//        taskDescription: document.getElementById('taskDescription').innerHTML,
+//        assignedTo: document.getElementById('assignedTo').innerHTML,
+//        dueDateId: document.getElementById('dueDateId').innerHTML,
+//        taskStatus: document.getElementById('taskStatus').innerHTML
+//      })
+//      this.currentId++;
+//     }
